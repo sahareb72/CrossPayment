6e185d5e94ff77d8a3839286d7de2cdc0e3f1840
sahareb72 <156338459+sahareb72@users.noreply.github.com>
2024-04-03 14:44:48 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,22 +1,238 @@
-window.addEventListener('load', function() {
-    if (typeof window.ethereum !== 'undefined') {
-        console.log('MetaMask is installed!');
-        const web3 = new Web3(window.ethereum);
-        
-        // Connect to the wallet
-        const connectWalletButton = document.getElementById('connectWalletButton');
-        connectWalletButton.addEventListener('click', async () => {
-            try {
-                await window.ethereum.request({ method: 'eth_requestAccounts' });
-                const accounts = await web3.eth.getAccounts();
-                document.getElementById('info').innerHTML = 'Wallet connected: ' + accounts[0];
-            } catch (error) {
-                console.error(error);
-            }
-        });
+// Replace these with your contract's details
+const contractABI = [
+	{
+		"inputs": [],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "_from",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "_value",
+				"type": "uint256"
+			}
+		],
+		"name": "Deposit",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "address",
+				"name": "sender",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "address",
+				"name": "recipient",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "amount",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "string",
+				"name": "currency",
+				"type": "string"
+			}
+		],
+		"name": "PaymentSent",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "amount",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "tax",
+				"type": "uint256"
+			}
+		],
+		"name": "TaxRate",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "pure",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "balances",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "deposit",
+		"outputs": [],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getSenderBalance",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "owner",
+		"outputs": [
+			{
+				"internalType": "address payable",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address payable",
+				"name": "receiver",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "amount",
+				"type": "uint256"
+			},
+			{
+				"internalType": "string",
+				"name": "currency",
+				"type": "string"
+			}
+		],
+		"name": "processPayment",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address payable",
+				"name": "_recipient",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_amount",
+				"type": "uint256"
+			},
+			{
+				"internalType": "string",
+				"name": "currency",
+				"type": "string"
+			}
+		],
+		"name": "sendPayment",
+		"outputs": [],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "amount",
+				"type": "uint256"
+			}
+		],
+		"name": "withdraw",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	}
+];
+const contractAddress = '0x798c31a096622Cc5fb05a03b0c88820ffEC50baF';
 
-        // Add more functionality here to interact with your smart contract
+let contract;
+let userAccount;
+
+window.addEventListener('load', async () => {
+    if (window.ethereum) {
+        window.web3 = new Web3(ethereum);
+        try {
+            await ethereum.request({ method: 'eth_requestAccounts' });
+            userAccount = (await web3.eth.getAccounts())[0];
+            contract = new web3.eth.Contract(contractABI, contractAddress);
+            
+            // Event listeners for buttons
+            document.getElementById('connectWallet').onclick = connectWallet;
+            document.getElementById('depositBtn').onclick = deposit;
+            document.getElementById('getBalanceBtn').onclick = getBalance;
+            document.getElementById('TaxRate').onclick = TaxRate;
+        } catch (error) {
+            console.error("Could not connect to wallet", error);
+        }
     } else {
-        document.getElementById('info').innerHTML = 'Please install MetaMask!';
+        console.log('Please install MetaMask!');
     }
 });
+        
+
+async function connectWallet() {
+    await ethereum.request({ method: 'eth_requestAccounts' });
+    userAccount = (await web3.eth.getAccounts())[0];
+    console.log('Wallet connected: ', userAccount);
+}
+
+async function deposit() {
+    const amount = web3.utils.toWei("0.01", "ether"); // Example amount to deposit
+    await contract.methods.deposit().send({ from: userAccount, value: amount });
+    console.log('Deposit made');
+}
+
+async function getBalance() {
+    const balance = await contract.methods.getSenderBalance().call({ from: userAccount });
+    document.getElementById('balance').innerText = "Balance: " + web3.utils.fromWei(balance) + " ETH";
+    console.log('Balance fetched');
+}
+
